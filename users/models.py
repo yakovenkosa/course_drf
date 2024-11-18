f5075838a5a651237a0c7372fb1ad045a1e78906
Sergey <sergey199433@gmail.com>
2024-11-18 22:47:50 +0400

    add model Payments

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -36,3 +38,47 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payments(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ("наличные", "наличные"),
+        ("перевод на счет", "перевод на счет"),
+    ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name="Пользователь",
+    )
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата оплаты")
+    course_paid = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Курс оплачен",
+    )
+    lesson_paid = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Урок оплачен",
+    )
+    payment_amount = models.DecimalField(
+        max_digits=9, decimal_places=2, verbose_name="Сумма оплаты"
+    )
+    payment_method = models.CharField(
+        max_length=50, choices=PAYMENT_METHOD_CHOICES, verbose_name="Cпособ оплаты"
+    )
+
+    def __str__(self):
+        return f"{self.user} - {self.payment_amount} - {self.payment_date}"
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+        ordering = ["-payment_date"]
