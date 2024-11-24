569e1ac835d9339e35b5ed3571b6858536844642
Sergey <sergey199433@gmail.com>
2024-11-24 20:51:41 +0400

    update urls.py in app users

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserProfileViewSet, PaymentsViewSet, UserViewSet
+from users.views import (
+    PaymentsViewSet,
+    UserCreateAPIView,
+    UserProfileViewSet,
+    UserViewSet,
+)
 
 app_name = UsersConfig.name
 
@@ -14,4 +21,11 @@ router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="login",
+    ),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
