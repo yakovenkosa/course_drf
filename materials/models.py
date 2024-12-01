d2e31e8687c2bb08ca8659fe6e577c5f5a33cef2
Sergey <sergey199433@gmail.com>
2024-12-01 19:13:26 +0400

    create model Subscription

--- materials/models.py
+++ materials/models.py
@@ -83,3 +83,11 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "course")
