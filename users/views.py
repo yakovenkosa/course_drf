61e3c741ca60456a726a12d7ac783bb40c41ee2c
Sergey <sergey199433@gmail.com>
2024-12-05 22:14:11 +0400

    update views.py for app materials and users

--- users/views.py
+++ users/views.py
@@ -6,9 +6,11 @@ from rest_framework.permissions import AllowAny
 
 from users.models import Payments, User
 from users.serializers import PaymentsSerializer, UserDetailSerializer, UserSerializer
+from users.services import create_stripe_price, create_stripe_session
 
 
 class UserCreateAPIView(CreateAPIView):
+    """Создание пользователя"""
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
@@ -20,6 +22,7 @@ class UserCreateAPIView(CreateAPIView):
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
+    """Просмотр профиля пользователя"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -31,13 +34,31 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 
 class PaymentsViewSet(viewsets.ModelViewSet):
+    """Просмотр платежей"""
     queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = ("payment_method", "paid_course", "paid_lesson")
+    filterset_fields = ("payment_method", "course_paid", "lesson_paid")
     ordering_fields = ("payment_date",)
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    """Просмотр пользователя"""
     queryset = User.objects.all()
     serializer_class = UserDetailSerializer
+
+class PaymentsCreateAPIView(CreateAPIView):
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
+
+    def perform_create(self, serializer):
+        payment = serializer.save(user=self.request.user)
+        payment_amount = payment.payment_amount
+        product = {
+            "Paid Course": payment.course_paid,
+        }
+        price = create_stripe_price(payment_amount, product)
+        session_id, payment_link = create_stripe_session(price)
+        payment.session_id = session_id
+        payment.payment_link = payment_link
+        payment.save()
