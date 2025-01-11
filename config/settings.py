8f9eee333b47097c08d59874df8007d8efd978a6
Sergey <sergey199433@gmail.com>
2025-01-11 20:53:56 +0400

    fix

--- config/settings.py
+++ config/settings.py
@@ -69,8 +69,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "shelter",
-        #"NAME": os.getenv("POSTGRES_DB"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         #"PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         #"HOST": os.getenv("POSTGRES_HOST"),
