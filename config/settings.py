31c6a36613617828036b9e4a0883f1f4eda15151
Sergey <sergey199433@gmail.com>
2025-01-11 20:53:03 +0400

    fix

--- config/settings.py
+++ config/settings.py
@@ -72,9 +72,9 @@ DATABASES = {
         "NAME": "shelter",
         #"NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        #"PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        #"HOST": os.getenv("POSTGRES_HOST"),
+        #"PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
