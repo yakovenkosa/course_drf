5b50ebbe695961f28610f4be7cd0dcdf4789b18a
Sergey <sergey199433@gmail.com>
2025-01-14 22:22:09 +0400

    fix settings.py

--- config/settings.py
+++ config/settings.py
@@ -107,6 +107,8 @@ STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATIC_ROOT = BASE_DIR / "static"
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
